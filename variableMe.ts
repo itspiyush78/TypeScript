2cd674d6fd9a7209e596210ce5dec01f0e614d66
itspiyush78 <itspiyush78@gmail.com>
2023-10-26 20:07:45 +0530

    any done

---
+++ variableMe.ts
@@ -0,0 +1,28 @@
+let greetings: string = "Hello Piyush";
+
+greetings.toLowerCase()
+console.log(greetings);
+
+// number
+
+let userId: number = 334455.3
+
+userId.toFixed()
+// boolean
+let isLoggedIn: boolean = false
+
+
+
+
+// any 
+
+let hero: String;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+  
+
+export {}
